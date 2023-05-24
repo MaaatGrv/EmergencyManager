01584155c09a3544b04c8fdb2d70d3e4c042e6ff
Mathis Gorvien <mathis.gorvien@cpe.fr>
2023-05-24 16:22:51 +0200

    init branch + add Static

---
+++ Static/src/test/java/com/FireFighter/Static/StaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FireFighter.Static;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
