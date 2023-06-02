a8b80d7d8555d8d83d3353e7d44a27887e09e494
Mathis Gorvien <mathis.gorvien@cpe.fr>
2023-06-02 08:51:48 +0200

    add vehicule microservice

--- Static/src/main/java/com/FireFighter/Static/controller/HomeController.java
+++ Static/src/main/java/com/FireFighter/Static/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.FireFighter.Static.controller;
+package com.firefighter.Static.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- vehicle/src/test/java/com/firefighter/vehicle/VehicleApplicationTests.java
+++ vehicle/src/test/java/com/firefighter/vehicle/VehicleApplicationTests.java
@@ -1,13 +1,12 @@
-package com.firefighter.Static;
+package com.firefighter.vehicle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StaticApplicationTests {
+class VehicleApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
-
 }
